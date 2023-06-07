6307890b6e9aa3e58eaa47bef35cac5a14569088
Michael Altfield <michael@michaelaltfield.net>
2023-06-06 20:55:33 -0400

    initial commit with license and bare script and github actions

---
+++ .github/workflows/stats-crawler.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+
+# this is so fucking unsafe https://rustup.rs/
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+which cargo
+cargo --version
+
+git clone https://github.com/LemmyNet/lemmy-stats-crawler.git
+cd lemmy-stats-crawler
+
+#~/.cargo/bin/cargo run -- --start-instances baraza.africa,lemmy.ml
+cargo run -- --start-instances baraza.africa,lemmy.ml
+
+exit 0
